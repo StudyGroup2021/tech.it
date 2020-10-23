d94eb11a1b39648c5382cee826e3456fc151a236
progg1992 <roggpierce@gmail.com>
2020-10-22 22:23:13 -0500

    Updated script.js and news-index.html

--- script.js
+++ script.js
@@ -2,44 +2,40 @@ $(document).ready(function(){
 function displayArticles() {
 // API key
 var APIKey = 'Y7bWWVONOPeRUHMryrKxY4TiGIWIKQMQ'
-//var country = $("").val().trim();
+
 var queryURL = "https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=" + APIKey;
-console.log(queryURL)
+
 $.ajax({
     url:queryURL,
     method: "GET"
 }).then(function(response) {
-    // console.log(response);
-    // var title = response.results[3].title;
-    // var description = response.results[3].abstract;
-    // var author = response.results[3].byline;
-    // var date = response.results[3].updated_date
-    // var articleURL = response.results[3].url;
-    // $('#title1').text(title);
-    // $('#description1').text(description);
-    // $('#author1').text(author);
-    // $('#date1').text(date);
-    // clickArticle(articleURL)
-    var article = response.results.length
+        var article = response.results.length
         var count = 0
         for (let i = 0; i < article - 26; i++) {
-            console.log('before' + count)
             var articleTitle = response.results[i].title
-            $('#title' + count).text(articleTitle)
+            var description = response.results[i].abstract;
+            var author = response.results[i].byline;
+            var date = response.results[i].updated_date;
+            var articleURL = response.results[i].url;
+            $('#title' + count).text(articleTitle);
+            $('#description' + count).text(description);
+            $('#author' + count).text(author);
+            $('#date' + count).text(date);
             count++
-            console.log('after' + count)
+            //end for loop    
         }
+    clickArticle(articleURL) 
+//end .then()
+})
+function clickArticle(articleURL) {
+    $('.article-row-content').on('click', function() {
+        $('#link').attr('href', articleURL);
+        console.log('articleURL' + articleURL)
+    })}
+
+//end displayAnswers()
 }
-)}
 displayArticles()
 
-// function clickArticle(articleURL) {
-//     $('.article-row-content').on('click', function() {
-//         $('#link1').attr('href', articleURL);
-//         console.log('articleURL' + articleURL)
-//     })}
-    
-    
-    
-// end
+// end document.reday()
 });
