0b4f41d3fa5d55bfff9a40a6dccac5974f8ad935
Megan Carnaghi <carnagh5@gmail.com>
2020-10-24 17:16:24 -0400

    Added images to jobs page, added footer to jobs and news pages, formatted quotes section on index.html

--- script.js
+++ script.js
@@ -3,46 +3,47 @@ $(document).ready(function () {
     // quotes list of quote and author
     quoteList = [
         {
-            quote: "Don't worry if it doesn't work right.  If everything did, you'd be out of a job.",
-            author: "(Mosher's Law of Software Engineering)"
+            quote: "\"Don't worry if it doesn't work right.  If everything did, you'd be out of a job.\"",
+            author: "- Mosher's Law of Software Engineering"
         },
         {
-            quote: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
-            author: "(Bill Gates)"
+            quote: "\"Measuring programming progress by lines of code is like measuring aircraft building progress by weight.\"",
+            author: "Bill Gates"
         },
         {
-            quote: "Good code is its own best documentation.",
-            author: "(Steve McConnell)"
+            quote: "\"Good code is its own best documentation.",
+            author: "Steve McConnell"
         },
         {
-            quote: "That's the thing about people who think they hate computers. What they really hate are lousy programmers.",
-            author: "(Larry Niven)"
+            quote: "\"That's the thing about people who think they hate computers. What they really hate are lousy programmers.\"",
+            author: "Larry Niven"
         },
         {
-            quote: "Everyone in this country should learn to program a computer... because it teaches you how to think.",
-            author: "(Steve Jobs)"
+            quote: "\"Everyone in this country should learn to program a computer... because it teaches you how to think.\"",
+            author: "Steve Jobs"
         },
         {
-            quote: "Whether you want to uncover the secrets of the universe, or you just want to pursue a career in the 21st century, basic computer programming is an essential skill to learn.",
-            author: "(Stephen Hawking)"
+            quote: "\"Whether you want to uncover the secrets of the universe, or you just want to pursue a career in the 21st century, basic computer programming is an essential skill to learn.\"",
+            author: "Stephen Hawking"
         },
         {
-            quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-            author: "(Martin Fowler)"
+            quote: "\"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.\"",
+            author: "Martin Fowler"
         }];
 
     function displayQuotes() {
 
         for (var i = 0; i < quoteList.length; i++) {
-            $(".quotes").empty();
-            var quoteP = $("<p>").addClass("class = 'quote-text'");
+            $(".quote-text").empty();
+            var quoteP = $("<p>").addClass("quote-text");
             $(quoteP).append(quoteList[i].quote);
-            $(".quotes").append(quoteP);
+            $(".quote-text").append(quoteP);
             
             console.log(quoteList[i].quote);
-            var authorP = $("<p>").addClass("class = 'quote-author'");
+            $(".quote-author").empty()
+            var authorP = $("<p>").addClass("quote-author");
             $(authorP).append(quoteList[i].author);
-            $(".quotes").append(authorP);
+            $(".quote-author").append(authorP);
             
 
         }
